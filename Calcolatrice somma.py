c720b7a2c51ea6839ec2288069cd825a4c352ece
Roberto <ramponiroberto1@gmail.com>
2024-01-22 17:20:33 +0100

    First commit

---
+++ Calcolatrice somma.py
@@ -0,0 +1,9 @@
+#Dichiarare le variabili di tipo float
+x = float(input('What's x? '))
+y = float(input('What's y? '))
+
+#Arrotonda la somma di x + y
+z = round(x + y)
+
+#Stampa il risultato con il separatore delle migliaia
+print(f'{z:.}')
---
+++ Hello.py
@@ -0,0 +1,5 @@
+#Ask user for their name, remove whitespaces from string and capitalize user's name
+name = input("What's your name? ").strip().title()
+
+#Say hello to user
+print("hello,", name)
